f5fc9c417ac727f532d73d8bb1db58a0ceb7e2e7
“junhuajia” <jiajunhua2013@gamil.com>
2020-12-25 16:36:18 +0800

    finised courseinfo of part1

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # fullstack
 
-(Full Stack Open 2020)[https://fullstackopen.com/en/]
+[Full Stack Open 2020](https://fullstackopen.com/en/)
+
+## TODO
+
+- [x] part0
+- [ ] part1
+- [ ] part2
+- [ ] part3
+- [ ] part4
+- [ ] part5
+- [ ] part6
+- [ ] part7
+- [ ] part8
+- [ ] part9
+- [ ] part10
+- [ ] part11
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+  const Header = ({ course }) => {
+    return <h1>{course.name}</h1>;
+  };
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        <Part partsItem={parts[0]} />
+        <Part partsItem={parts[1]} />
+        <Part partsItem={parts[2]} />
+      </div>
+    );
+  };
+  const Total = ({ parts }) => {
+    return (
+      <p>
+        Number of exercises{' '}
+        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
+    );
+  };
+
+  const Part = ({ partsItem }) => {
+    return (
+      <p>
+        {partsItem.name} {partsItem.exercises}
+      </p>
+    );
+  };
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
